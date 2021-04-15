f389294af6e39418c28b22f27caacfa583519ad1
rehan7594 <rehansdm94@gmail.com>
2021-04-15 12:22:59 -0700

    Added: Distance between two points

--- com/linecomparisoncomputation/LineComparisonComputation.java
+++ com/linecomparisoncomputation/LineComparisonComputation.java
@@ -1,9 +1,22 @@
 package com.linecomparisoncomputation;
 
+import java.util.Scanner;
+
 public class LineComparisonComputation {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation");
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of x1: ");
+		double x1=s.nextDouble();
+		System.out.println("Enter the value of y1: ");
+		double y1=s.nextDouble();
+		System.out.println("Enter the value of x2: ");
+		double x2=s.nextDouble();
+		System.out.println("Enter the value of y2: ");
+		double y2=s.nextDouble();
+		double dist=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("Distance between two points: "+dist);
 	}
 
 }
