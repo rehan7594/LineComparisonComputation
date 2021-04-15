c0aa3ce9523bd84b96bf97991a24d96027776ac4
rehan7594 <rehansdm94@gmail.com>
2021-04-15 11:50:52 -0700

    Added: Welcome message

---
+++ com/linecomparisoncomputation/LineComparisonComputation.java
@@ -0,0 +1,9 @@
+package com.linecomparisoncomputation;
+
+public class LineComparisonComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+
+}
