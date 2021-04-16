588e200e5aadfd723d5b54af91bf45d95941c210
rehan7594 <rehansdm94@gmail.com>
2021-04-16 08:15:26 -0700

    Added: Line and point as well as equals and compareTO method

--- com/linecomparisoncomputation/LineComparisonComputation.java
+++ com/linecomparisoncomputation/LineComparisonComputation.java
@@ -1,46 +1,80 @@
 package com.linecomparisoncomputation;
 
-import java.util.Scanner;
+public class LineComparisonComputation
+{
+	// create function for equal()
+		public static void equal() 
+		{
+			// member Variables
+			int x1, x2, y1, y2, x3, y3; // line coordinates
+			int line1, line2;
+			x1 = 1; // initialize value
+			y1 = 1;
+			x2 = 0;
+			y2 = 5;
+			x3 = 9;
+			y3 = 0;
 
-public class LineComparisonComputation {
+			// calculate length of a line
+			line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			line2 = (int) Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+			// cover the double into string and store the value length1
+			String s2 = String.valueOf(line2);
+			String s1 = String.valueOf(line1);
 
-	public static void main(String[] args) 
-	{
-		Scanner s=new Scanner(System.in);
-		
-		//Distance between two points for line1
-		System.out.println("Enter the value of x1: ");
-		double x1=s.nextDouble();
-		System.out.println("Enter the value of y1: ");
-		double y1=s.nextDouble();
-		System.out.println("Enter the value of x2: ");
-		double x2=s.nextDouble();
-		System.out.println("Enter the value of y2: ");
-		double y2=s.nextDouble();
-		double dist1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		
-		
-		//Distance between two points for line2
-		System.out.println("Enter the value of x3: ");
-		double x3=s.nextDouble();
-		System.out.println("Enter the value of y3: ");
-		double y3=s.nextDouble();
-		System.out.println("Enter the value of x4: ");
-		double x4=s.nextDouble();
-		System.out.println("Enter the value of y4: ");
-		double y4=s.nextDouble();
-		double dist2=Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
-		
-		if (Double.compare(dist1, dist2) > 0) 
-		      System.out.println("The length dist1 is greater than length dist2");
-		   
-		else if (Double.compare(dist1, dist2) < 0) 
-		     System.out.println("The length dist1 is less than length dist2");
-		   
-		else 
-		     System.out.println("The length dist1 is equal to length dist2");
-		
-		
+			// checking lines are equal or not
+			System.out.println(s1.equals(s2));
+		}
+
+		//// create function for CompareTo()
+		public static void compare()
+		{
+
+			// member Variables
+			int x1, x2, y1, y2, x3, y3;
+			int line1, line2;
+			x1 = 1;// initialize value
+			y1 = 2;
+			x2 = 3;
+			y2 = 4;
+			x3 = 8;
+			y3 = 0;
+			// calculate of length of line
+			line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			line2 = (int) Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+
+			// print line 1 and line 2 value
+			System.out.println("Line1  Length = " + line1);
+			System.out.println("Line2  Length = " + line2);
+
+			// creating a object of integer class
+			Integer a = new Integer(line1);
+			Integer b = new Integer(line2);
+
+			// comparing the lines length using compareTo() method
+			int compare = a.compareTo(b);
+
+			// checking lines are equal,greater or smaller than one another
+			if (compare > 0)
+			{
+				System.out.println("line1 is greater than line");
+			} 
+			else if (compare < 0) 
+			{
+				System.out.println("line1 is less than line2");
+			} 
+			else
+			{
+				System.out.println("line1 is equal to line2");
+			}
+		}
+
+		public static void main(String[] args)
+		{
+			// function calling
+			compare();
+			equal();
+
+		}
+
 	}
-
-}
